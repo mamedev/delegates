3852ada55b7695382014ea2a408e984a754dbf87
Miodrag Milanovic <mmicko@gmail.com>
2016-03-16 19:34:34 +0100

    Works now on MIPS as well, tested on Creator Ci20

--- src/delegate.cpp
+++ src/delegate.cpp
@@ -39,7 +39,7 @@ delegate_mfp::raw_mfp_data delegate_mfp::s_null_mfp = { {0 }};
 
 delegate_generic_function delegate_mfp::convert_to_generic(delegate_generic_class *&object) const
 {
-#if defined(__arm__) || defined(__ARMEL__) || defined(__aarch64__)
+#if defined(__arm__) || defined(__ARMEL__) || defined(__aarch64__) || defined(__MIPSEL__) || defined(__mips_isa_rev) || defined(__mips64)
 	// apply the "this" delta to the object first
 	object = reinterpret_cast<delegate_generic_class *>(reinterpret_cast<std::uint8_t *>(object));
 
