f394acbf4cf9a475a9b0f2848e598230e265c79b
Miodrag Milanovic <mmicko@gmail.com>
2017-02-19 11:55:56 +0100

    Cleanup

--- main.cpp
+++ main.cpp
@@ -4,22 +4,15 @@
 #include "src/delegate.h"
 #include <functional>
 
-
-#define BENCHMARK 0
-
-class running_machine
+class simple_dummy_class
 {
 public:
-	running_machine() {}
+	simple_dummy_class() {}
 };
 
 // Declare some functions of varying complexity...
-void SimpleStaticFunction(running_machine &, int num) {
+void SimpleStaticFunction(simple_dummy_class &, int num) {
 	printf("In SimpleStaticFunction. Num=%d\n", num);
-}
-
-void SimpleVoidFunction(running_machine &) {
-	printf("In SimpleVoidFunction with no parameters.\n");
 }
 
 class CBaseClass {
@@ -35,16 +28,13 @@ public:
 	{
 		printf("In ConstSimpleMemberFunction in %s. Num=%d\n", m_name, num);
 	}
-	int SimpleMemberFunctionReturnsInt(int num) {
-		printf("In SimpleMemberFunction in %s. Num=%d\n", m_name, num); return -1;
-	}
 	void ConstMemberFunction(int num) const {
 		printf("In ConstMemberFunction in %s. Num=%d\n", m_name, num);
 	}
 	virtual void SimpleVirtualFunction(int num) {
 		printf("In SimpleVirtualFunction in %s. Num=%d\n", m_name, num);
 	}
-	static void StaticMemberFunction(running_machine &, int num) {
+	static void StaticMemberFunction(simple_dummy_class &, int num) {
 		printf("In StaticMemberFunction. Num=%d\n", num);
 	}
 };
@@ -75,30 +65,6 @@ public:
 };
 
 
-class MyClass
-{
-public:
-	MyClass() { i = 0; }
-	virtual ~MyClass() { }
-
-	void docount2(int j) { i += j; }
-	virtual void docount(int j) { i+=j; }
-	int get() { return i; }
-private:
-	int i;
-};
-
-class MyClass2 : public MyClass
-{
-public:
-	MyClass2() : MyClass() { }
-	virtual ~MyClass2() { }
-	virtual void docount(int) {  }
-};
-
-#define FUNC(x) &x, #x
-
-typedef delegate<void(void)> VoidDelegate;
 typedef delegate<void(int)> MyDelegate;
 
 template<class _FunctionClass>
@@ -112,32 +78,15 @@ void dump_mfp(_FunctionClass *object, void (_FunctionClass::*mfp)(int num))
 		printf("  %08X\n", (std::uint32_t)ptr[i]);
 }
 
-template<class _FunctionClass>
-void dump_addr(const char *, _FunctionClass *, void (_FunctionClass::*)(int))
-{
-	//printf("%s Addr = %08x\n", text, (void*)(object->*mfp));
-}
-
-
 void dump_vtable(const char *name, void *ptr)
 {
 	void **vtable = *reinterpret_cast<void ***>(ptr);
 	printf("%s: vtable @ %p = %p %p %p %p\n", name, *vtable, vtable[0], vtable[1], vtable[2], vtable[3]);
 }
-void print_num(int i)
+
+void print_num_std_function(int i)
 {
 	printf("In std:function. Num=%d\n",i);
-}
-
-int read_num()
-{
-	return 10;
-}
-
-int st = 0;
-void static_count(int j)
-{
-	st += j;
 }
 
 int main(int, char**)
@@ -181,13 +130,13 @@ int main(int, char**)
 	printf("Using fast internal MSVC delegates\n");
 #endif
 
-	running_machine machine;
+	simple_dummy_class machine;
 	MyDelegate funclist[12]; // delegates are initialized to empty
 	CBaseClass a("Base A");
 	CBaseClass b("Base B");
 	CDerivedClass c;
 	CDerivedClass d;
-#if !BENCHMARK	
+
 	dump_vtable("A", &a);
 	dump_vtable("D", &d);
 
@@ -198,10 +147,6 @@ int main(int, char**)
 	dump_mfp(&d, &CDerivedClass::SimpleDerivedFunction);
 	dump_mfp(&d, &CDerivedClass::TrickyVirtualFunction);
 
-	dump_addr("CBaseClass::SimpleMemberFunction",&a, &CBaseClass::SimpleMemberFunction);
-	dump_addr("CBaseClass::SimpleVirtualFunction",&b, &CBaseClass::SimpleVirtualFunction);
-	dump_addr("CDerivedClass::TrickyVirtualFunction",&c, &CDerivedClass::TrickyVirtualFunction);
-	dump_addr("CDerivedClass::SimpleDerivedFunction",&c, &CDerivedClass::SimpleDerivedFunction);
 	// Binding a simple member function
 	printf("funclist[0] = MyDelegate(&CBaseClass::SimpleMemberFunction, &a);\n");
 	funclist[0] = MyDelegate(&CBaseClass::SimpleMemberFunction, &a);
@@ -225,7 +170,7 @@ int main(int, char**)
 	printf("funclist[8] = MyDelegate(&CDerivedClass::SimpleDerivedFunction,&c);\n");
 	funclist[8] = MyDelegate(&CDerivedClass::SimpleDerivedFunction,&c);
 	printf("funclist[9] = std::function<void(int)>\n");
-	std::function<void(int)> func = print_num;
+	std::function<void(int)> func = print_num_std_function;
 	funclist[9] = MyDelegate(func);
 	printf("funclist[10] = [](int a) -> void {}\n");
 	funclist[10] = MyDelegate([](int a) -> void { printf("In lambda. Num=%d\n",a); });
@@ -238,83 +183,6 @@ int main(int, char**)
 		}
 	}
 
-#else
-	typedef delegate<void(int j)> driver_callback_delegate;
-
-	MyClass2 mc;
-	driver_callback_delegate md = driver_callback_delegate(&MyClass2::docount, &mc);
-	
-	printf("Benchmarking virtual call : ");
-	{
-		auto before = std::chrono::high_resolution_clock::now(); ;
-		for (int i = 0; i < 100000000; i++)
-		{
-			md(i);
-		}
-		auto after = std::chrono::high_resolution_clock::now(); ;
-		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before);
-		printf("%lld\n", elapsed.count());
-	}
-	
-	int i = 0;
-	driver_callback_delegate pda = driver_callback_delegate([&](int j) {  i += j; });
-
-	printf("Benchmarking lambda call : ");
-	{
-		auto before = std::chrono::high_resolution_clock::now(); ;
-		for (int i = 0; i < 100000000; i++)
-		{
-			pda(i);
-		}
-		auto after = std::chrono::high_resolution_clock::now(); ;
-		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before);
-		printf("%lld\n", elapsed.count());
-	}
-
-	driver_callback_delegate md2 = driver_callback_delegate(&static_count);
-
-	printf("Benchmarking static call : ");
-	{
-		auto before = std::chrono::high_resolution_clock::now(); ;
-		for (int i = 0; i < 100000000; i++)
-		{
-			md2(i);
-		}
-		auto after = std::chrono::high_resolution_clock::now(); ;
-		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before);
-		printf("%lld\n", elapsed.count());
-	}
-
-	driver_callback_delegate md3 = driver_callback_delegate(std::function<void(int j)>(static_count));
-	printf("Benchmarking std::function call : ");
-	{
-		auto before = std::chrono::high_resolution_clock::now(); ;
-		for (int i = 0; i < 100000000; i++)
-		{
-			md3(i);
-		}
-		auto after = std::chrono::high_resolution_clock::now(); ;
-		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before);
-		printf("%lld\n", elapsed.count());
-	}
-
-	MyClass2 mcbind;
-	auto f = std::bind(&MyClass2::docount, (MyClass2*)&mcbind, std::placeholders::_1);
-
-	driver_callback_delegate md4 = driver_callback_delegate(f);
-	printf("Benchmarking std::bind call : ");
-	{
-		auto before = std::chrono::high_resolution_clock::now(); ;
-		for (int i = 0; i < 100000000; i++)
-		{
-			md4(i);
-		}
-		auto after = std::chrono::high_resolution_clock::now(); ;
-		auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(after - before);
-		printf("%lld\n", elapsed.count());
-	}
-
-#endif
 	printf("Done\n");
 	return 0;
 
